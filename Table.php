b2aa027268d07b169b0032a471f6f6e38e392825
Greg <soults@live.ru>
2013-01-29 13:32:25 +0400

    Update Table.php
    29.01.2013:
    - Добавлена защита от SQL-инъекции;
    - Обновлены функции для выборки строк из БД;
    - Исправленный обработчик ошибок PDO

--- Table.php
+++ Table.php
@@ -1,89 +1,70 @@
-<?php 
+<?php
 
-    class Table {
-		
-		public $db;
-		protected $table;
-		
-		/**
-		 * 
-		 * @param sting $table
-		 * 
-		 * Init Table.php class
-		 * 
-		 */
-		
-		public function __construct($table) {
-			$pdo = new PDO('mysql:host=localhost;dbname=alloders', 'soults', '19962728');
-			$pdo->query('set names utf8');
-			$this->db = $pdo;
-			$this->table = $table;
-		}
-		
-		/**
-		 * Get rows from database
-		 * 
-		 * @param string $where
-		 * @param int $limit
-		 * @param bool $row
-		 * @return $d:
-		 * 
-		 */
-		
-		public function select($where = null, $limit = null, $row = false) {
-			$query = 'SELECT * FROM `' . $this->table . '`';
-			if($where != null) $query .= ' WHERE ' . $where;
-			if($limit != null) $query .= ' LIMIT ' . $limit;
-			try {
-				$stmt = $this->db->query($query);
-				if($row == false) {
-					$d = $stmt->fetchAll(PDO::FETCH_OBJ);
-				} else {
-					$d = $stmt->fetch(PDO::FETCH_OBJ);
-				}
-			} catch (PDOException $e) {
-				echo $e->getMessage();
-			}
-			return $d;
-		}
-		
-		/**
-		 * Delete or count of rows from Database
-		 * 
-		 * @param string $where
-		 * @return boolean
-		 * 
-		 */
-		
-		public function delete($where) {
-			if(!is_array($where)) {
-				$query = "DELETE FROM `" . $this->table . '` WHERE ' . $where;
-				try {
-					$this->db->prepare($query)->execute();
-				} catch (PDOException $e) {
-					echo $e->getMessage();
-				}
-			} else {
-				foreach($where as $c) {
-					$query = "DELETE FROM `" . $this->table . '` WHERE ' . $c . ';' . PHP_EOL;
-					try {
-						$this->db->prepare($query)->execute();
-					} catch (PDOException $e) {
-						echo $e->getMessage();
-					}
-				}
-			}
-			return true;
-		}
-		
-		/**
-		 * Close connection
-		 */
-		
-		public function __destruct() {
-			$this->db = null;
-		}
-		
-	}
+class Table {
 
-?>
+    public $db;
+    public $table;
+
+    public function __construct($table) {
+        $this->db = new PDO('mysql:host=localhost;dbname=dbname', 'mysqluser', 'mysqlpass');
+        $this->db->query('SET NAMES utf8');
+        $this->table = $table;
+    }
+    
+    /**
+     * Функция для получения одной строки из базы данных
+     * Если не указан параметр WHERE в строке query, то выбирается
+     * первая запись из таблицы
+     * 
+     * @param string $query
+     * @return array
+     */
+
+    public function getRow($query) {
+        $args = func_get_args();
+        array_shift($args);
+        try {
+            $st = $this->db->prepare($query);
+            $st->execute($args);
+            $d = $st->fetch();
+            if (!$d) {
+                print_r("Не найдено ни одной записи!");
+            }
+            return $d;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+    
+    /**
+     * Функция для полученя всех строк из базы данных
+     * Параметры выборки можно перечислить через запятую, после
+     * указания строки query.
+     * 
+     * Например: $this->getAllRows("SELECT * FROM `table` WHERE `param` = ? AND `param_two` = ?", $param1, $param2);
+     * 
+     * @param string $query
+     * @return array
+     */
+
+    public function getAllRows($query) {
+        $args = func_get_args();
+        array_shift($args);
+        try {
+            $st = $this->db->prepare($query);
+            $st->execute($args);
+            $d = $st->fetchAll();
+            if (!$d) {
+                print_r("Не найдено ни одной записи!");
+            }
+            return $d;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+    
+    public function __destruct() {
+    	$this->db = null;
+    }
+
+}
